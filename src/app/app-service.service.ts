5aa572adb7838f7cb865210b851e0f9a0171cf86
Camilla Damasceno <camilla.damasceno@dtidigital.com.br>
2019-10-02 22:02:53 -0300

    deu certooooo

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -2,12 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS', 'Accept': 'application/json',
-  'content-type': 'application/json'})
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  responseType: 'text' as 'json'
 };
 
-@Injectable({
+@Injectable({cer!!
   providedIn: 'root'
 })
 export class AppServiceService {
@@ -16,6 +15,6 @@ export class AppServiceService {
   constructor(private http: HttpClient) { }
 
   corrigirProva(key: string, test: string) {
-    return this.http.post<any>(this.baseUrl, { "key": key, "test": test }, httpOptions);
+    return this.http.post<any>(this.baseUrl, { key, test }, httpOptions);
   }
 }
